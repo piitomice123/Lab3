00dcdbb39b73baa3b42e9920790832fabdd5bee7
piitomice123 <piitomice-123@hotmail.co.th>
2017-12-20 14:20:06 +0700

    gg

---
+++ src/main/java/Rmuti/lab3/Lab3/TestArrayList.java
@@ -0,0 +1,17 @@
+package Rmuti.lab3.Lab3;
+
+public class TestArrayList {
+
+	public static void main(String[] args) {
+		ArrayList lst =new ArrayList();
+		lst.add(0, "10");
+		lst.add(1, "11");
+		lst.add(2, "13");
+		lst.add(3, "12");
+		lst.add(4, "20");
+		lst.add(5, "60");
+		lst.add(6, "80");
+		System.out.println(lst);
+	}
+
+}
